4faa2287178401f15b99560de3785bc4d6a2f6e7
dkarmy12 <55491427+dkarmy12@users.noreply.github.com>
2021-07-18 20:06:25 +0530

    Added New News Cards section, added animation in navbar, maps in footer, animated about us section

--- assets/js/base.js
+++ assets/js/base.js
@@ -11,3 +11,24 @@
 //       self.direction === -1 ? showAnim.play() : showAnim.reverse()
 //     }
 //   });
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 17.5947, lng: 78.1230 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
+let tl = new TimelineMax({paused:true, reversed:true});
+tl.from('.dropdown-menu', 0.25, {height: 0, ease: Power2.easeOut, opacity: 0})
+tl.staggerFrom('.dropdown-menu > li', .2, {y: -20, opacity: 0}, .015);
+
+document.getElementById("navbarDropdown").addEventListener("click", function(){
+    tl.reversed() ? tl.play() : tl.reverse(0);
+  });
--- assets/js/index.js
+++ assets/js/index.js
@@ -4,15 +4,24 @@ function logger(image)
     newsImg.src = `/assets/images/${image}`;
 }
 gsap.registerPlugin(ScrollTrigger);
-gsap.from("#row1", {
-    scrollTrigger: "#row1",
-    y: 200,
-    duration: 0.5,
+ScrollTrigger.defaults({
+    toggleActions: "play complete play reset"
+    });
+gsap.from("#link", {
+    scrollTrigger: "#head",
+    y: 100,
+    delay:0.5,
+    duration: 0.4,
+    stagger: 0.2,
+    opacity:0,
     ease:"power1.inOut"
   });
-  gsap.from("#row2", {
-    scrollTrigger: "#row2", 
-    y: 200,
+  gsap.from("#abtus", {
+    scrollTrigger: "#abtus",
+    y: 100,
     duration: 0.5,
-    ease:"power1.inOut"
+    delay:0.1,
+    stagger: 0.2,
+    opacity:0,
+    ease:"power2.inOut"
   });
