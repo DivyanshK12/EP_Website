c43c2c28a30e9b257c20ec6e993c70c6650d6c31
dkarmy12 <55491427+dkarmy12@users.noreply.github.com>
2021-08-03 14:56:58 +0530

    Added year tabs in betch in programs, changed font family

--- assets/js/programs.js
+++ assets/js/programs.js
@@ -1,4 +1,15 @@
-function empty()
+window.onload = () => {
+    sessionStorage.setItem('prevProg', 2020);
+}
+function change(year)
 {
-    console.info("Just a function to prevent an empty file");
+    let prev = sessionStorage.getItem('prevProg', 2020);// integer like 2019, 2018
+    if(year!=prev)
+    {
+        document.getElementById(year).classList.remove('d-none');
+        document.getElementById(`nav-${year}`).classList.add('active');
+        document.getElementById(prev).classList.add('d-none');
+        document.getElementById(`nav-${prev}`).classList.remove('active');
+        sessionStorage.setItem('prevProg', year);
+    }
 }
