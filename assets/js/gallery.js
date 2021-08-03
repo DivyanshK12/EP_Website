dbc73529a5f3f9f855800d8b1c6cb97c4a9a6293
dkarmy12 <55491427+dkarmy12@users.noreply.github.com>
2021-08-03 12:45:51 +0530

    Updated Gallery, added student's page

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -22,35 +22,3 @@ function change(id)
       sessionStorage.setItem('prev', id);
   }
 }
-
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-function zoom(p) {
- var x = document.getElementsByClassName("myImg");
- var i;
- for (i = 0; i < x.length; i++)
- {
-    if(x[i] == p)
-    {
-      modal.style.display = "block";
-      modalImg.src = x[i].src;
-      captionText.innerHTML = x[i].alt;
-    }
-    
- }
-}
-
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
