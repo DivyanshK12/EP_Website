8feb249e43fbea4202102523d0872e3e24ea3fc8
dkarmy12 <55491427+dkarmy12@users.noreply.github.com>
2021-08-08 23:24:08 +0530

    Updated Gallery

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,24 +1,22 @@
-let items = document.querySelectorAll('.sidebarli');
+let items = document.querySelectorAll(".sidebarli");
 
-items.forEach(item => {
-  item.addEventListener('click', () => {
-     items.forEach(item => item.classList.remove('active'))
-      item.classList.add('active')
-  })
-})
+items.forEach((item) => {
+  item.addEventListener("click", () => {
+    items.forEach((item) => item.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
 
 window.onload = () => {
-  sessionStorage.setItem('prev', 1);
-}
-function change(id)
-{
-  let prev = sessionStorage.getItem('prev', 1);// integer like 1, 2
-  if(id!=prev)
-  {
-      document.getElementById(id).classList.remove('d-none');
-      document.getElementById(`gal-${id}`).classList.add('active');
-      document.getElementById(prev).classList.add('d-none');
-      document.getElementById(`gal-${prev}`).classList.remove('active');
-      sessionStorage.setItem('prev', id);
+  sessionStorage.setItem("prev", 1);
+};
+function change(id) {
+  let prev = sessionStorage.getItem("prev", 1); // integer like 1, 2
+  if (id != prev) {
+    document.getElementById(id).classList.remove("d-none");
+    document.getElementById(`gal-${id}`).classList.add("active");
+    document.getElementById(prev).classList.add("d-none");
+    document.getElementById(`gal-${prev}`).classList.remove("active");
+    sessionStorage.setItem("prev", id);
   }
 }
