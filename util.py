06a89230d05a23ea8af505bf376b1a8a97fdefba
dkarmy12 <55491427+dkarmy12@users.noreply.github.com>
2021-08-13 15:29:15 +0530

    Minor bug fixes and improvements

--- util.py
+++ util.py
@@ -15,6 +15,9 @@ def csv_to_yml(csv_filename : str = "in.csv", yml_filename : str = "out.yml") ->
                 outfile.write("\n")
     print(f"Completed writing to {yml_filename}")
 
+def yml_to_csv(yml_filename : str = "in.yml", csv_filename : str = "out.csv") -> None :
+    pass
+
 def main() -> None:
     input_count = len(sys.argv)
     if input_count >= 3 :
@@ -26,3 +29,8 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+
+# incase Python version is less than 3.6 (Before f-strings were introduced), copy the following to make it work:
+# Line 11 : outfile.write("- id : {}\n".format(index))
+# Line 13 : outfile.write(f"  {} : {}\n".format(key, value))
+# Line 16 : print(f"Completed writing to {}".format(yml_filename))
