53cb6f0c2bb136f6f6c61c01a5e9bbdceed9a1eb
dkarmy12 <55491427+dkarmy12@users.noreply.github.com>
2021-08-16 19:11:15 +0530

    Added Main page text, Updated Gallery, Updated Faculty in ECMP

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,12 +1,3 @@
-let items = document.querySelectorAll(".sidebarli");
-
-items.forEach((item) => {
-  item.addEventListener("click", () => {
-    items.forEach((item) => item.classList.remove("active"));
-    item.classList.add("active");
-  });
-});
-
 window.onload = () => {
   sessionStorage.setItem("prev", 1);
 };
@@ -20,3 +11,32 @@ function change(id) {
     sessionStorage.setItem("prev", id);
   }
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+  document.querySelectorAll('.sidebar .nav-link').forEach(function(element){
+    
+    element.addEventListener('click', function (e) {
+
+      let nextEl = element.nextElementSibling;
+      let parentEl  = element.parentElement;	
+
+        if(nextEl) {
+            e.preventDefault();	
+            let mycollapse = new bootstrap.Collapse(nextEl);
+            
+            if(nextEl.classList.contains('show')){
+              mycollapse.hide();
+            } else {
+                mycollapse.show();
+                // find other submenus with class=show
+                var opened_submenu = parentEl.parentElement.querySelector('.submenu.show');
+                // if it exists, then close all of them
+                if(opened_submenu){
+                  new bootstrap.Collapse(opened_submenu);
+                }
+            }
+        }
+    }); // addEventListener
+  }) // forEach
+}); 
+// DOMContentLoaded  end
