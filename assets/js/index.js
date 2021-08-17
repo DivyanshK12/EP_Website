27e3e3ea31b7d0d4e679f8f156290b014985025a
dkarmy12 <55491427+dkarmy12@users.noreply.github.com>
2021-08-17 20:27:14 +0530

    Modified Reserch area pages to be more responsive, modified landing page about us section

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,6 +12,30 @@ function logger(id)
       sessionStorage.setItem('prevSlide', field);
   }
 }
+
+function viewMore()
+{
+  let row = document.getElementById("moreInfo");
+  let toggle = document.getElementById("infoToggle");console.log(row.classList);
+  if(row.classList.contains("d-none"))
+  {
+    row.classList.remove("d-none");
+    toggle.innerText = "Read Less...";
+    gsap.from("#moreInfo", {
+      y: -100,
+      duration: 0.5,
+      opacity:0,
+      ease:"power2.inOut"
+    });
+  }
+  else
+  {
+    row.classList.add("d-none");
+    toggle.innerText = "Read More..."
+  }
+  console.log("Toggled");
+  return false;
+}
 gsap.registerPlugin(ScrollTrigger);
 ScrollTrigger.defaults({
     toggleActions: "play complete play reset"
